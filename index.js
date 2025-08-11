757600fc74fae9ec44d56a7c743a1dd92e067383
Ankitchanal-eng <ankitchanal43@gmail.com>
2025-08-11 21:51:52 +0530

    Add authenticated EP

--- index.js
+++ index.js
@@ -57,6 +57,28 @@ app.post("/signin", function(req, res) {
     console.log(users)
 })
 
+app.get("/me", function(req, res) {
+    const token = req.headers.token
+    let foundUser = null;
+
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].token == token) {
+            foundUser = users[i]
+        }
+    }
+
+    if (foundUser) {
+        res.json({
+            username: foundUser.username,
+            password: foundUser.password
+        })
+    } else {
+        res.json({
+            message: "token invalid"
+        })
+    }
+})
+
 app.listen(3000, () => {
     console.log("The server is running on http://localhost:3000");
 });
